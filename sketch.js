805b2a46432c990b0b9206af2da4d7889f342fc9
Nath789456 <70635839+Nath789456@users.noreply.github.com>
2020-10-21 10:51:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,200 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var polygon;
+var slingShot;
+var polygon_img;
+var bg;
+var score=0;
+function preload(){
+  polygon_img=loadImage("polygon.png");
+    getBackgroundImg();
+   // backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup() {
+  createCanvas(900,400);
+
+ 
+
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  stand1 = new Stand(390,300,250,10);
+  stand2 = new Stand(700,200,200,10);
+ 
+
+  block1 = new Block(300,275,30,40);
+  //console.log(block1);
+  block2 = new Block(330,275,30,40);
+  block3 = new Block(360,275,30,40);
+  block4 = new Block(390,275,30,40);
+  block5 = new Block(420,275,30,40);
+  block6 = new Block(450,275,30,40);
+  block7 = new Block(480,275,30,40);
+  
+  block8 = new Block(330,235,30,40);
+  block9 = new Block(360,235,30,40);
+  block10 = new Block(390,235,30,40);
+  block11 = new Block(420,235,30,40);
+  block12 = new Block(450,235,30,40);
+  
+  block13 = new Block(360,195,30,40);
+  block14 = new Block(390,195,30,40);
+  block15 = new Block(420,195,30,40);
+  
+  block16 = new Block(390,155,30,40);
+
+  blocks1 = new Block(640,175,30,40);
+  blocks2 = new Block(670,175,30,40);
+  blocks3 = new Block(700,175,30,40);
+  blocks4 = new Block(730,175,30,40);
+  blocks5 = new Block(760,175,30,40);
+
+  blocks6 = new Block(670,135,30,40);
+  blocks7 = new Block(700,135,30,40);
+  blocks8 = new Block(730,135,30,40);
+
+  blocks9 = new Block(700,95,30,40);
+
+
+  polygon = Bodies.circle(50,200,20);
+  World.add(world,polygon);
+
+  slingShot = new Slingshot(this.polygon,{x:100,y:200});
+
+}
+function draw() {
+  //background(47,79,79);
+  if(backgroundImg)
+  background(backgroundImg);
+  Engine.update(engine);
+
+  fill("Yellow");
+  text("SCORE= "+score,750,40);
+  
+  textSize(20);
+  fill("Yellow");
+  text("Press SpaceKey to start hitting the blocks again!!",200,30);
+
+ 
+  //Engine.update(engine);
+   
+  ground.display();
+  stand1.display();
+  stand2.display();
+  strokeWeight(3);
+  stroke(15);
+  fill("pink");
+  block1.display();
+  block2.display();
+  block3.display();
+  block4.display();
+  block5.display();
+  block6.display();
+  block7.display();
+  fill("cyan");
+  block8.display();
+  block9.display();
+  fill("yellow");
+  block10.display();
+  fill("cyan");
+  block11.display();
+  block12.display();
+  fill("lavender");
+  block13.display();
+  block14.display();
+  block15.display();
+  fill("golden");
+  block16.display();
+  fill("pink");
+  blocks1.display();
+  blocks2.display();
+  blocks3.display();
+  blocks4.display();
+  blocks5.display();
+  fill("cyan");
+  blocks6.display();
+  blocks7.display();
+  blocks8.display();
+  fill("lavender")
+  blocks9.display();
+  fill("gold");
+
+  imageMode(CENTER)
+  image(polygon_img ,polygon.position.x,polygon.position.y,40,40);
+
+  slingShot.display();
+
+
+  block1.score();
+  block2.score();
+  block3.score();
+  block4.score();
+  block5.score();
+  block6.score();
+  block7.score();
+  block8.score();
+  block9.score();
+  block10.score();
+  block11.score();
+  block12.score();
+  block13.score();
+  block14.score();
+  block15.score();
+
+  blocks1.score();
+  blocks2.score();
+  blocks3.score();
+  blocks4.score();
+  blocks5.score();
+  blocks6.score();
+  blocks7.score();
+  blocks8.score();
+  blocks9.score();
+
+
+}
+function mouseDragged(){
+  Matter.Body.setPosition(this.polygon,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
+}
+
+function keyPressed(){
+  if(keyCode===32){
+    slingShot.attach(this.polygon);
+  }
+}
+
+
+
+async function getBackgroundImg(){
+  var response= await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+  var responseJSON= await response.json();
+//  console.log(responseJSON.datetime);
+  var datetime= responseJSON.datetime;
+  var hour= datetime.slice(11,13);
+  console.log(hour);
+
+  if(hour>=06 && hour<=21){
+    bg= "bg.png";
+  }
+ else{
+     bg="bg2.jpg";
+ }
+backgroundImg= loadImage(bg);
+}
+
+ function score(){
+  if(this.visibility<0 && this.visibility>=105){
+    score++;
+  }
+}
